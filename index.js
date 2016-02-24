00e061840f7410a7d8fdb1b10a5670ed232e2d92
bounceme <mac@Macs-MacBook-Pro.local>
2016-02-23 19:09:40 -0800

    Style changes

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var bridge;
-var ROOM_ID = "!YiuxjYhPLIZGVVkFjT:localhost"; // this room must have join_rules: public
-var Cli = require("matrix-appservice-bridge").Cli;
-var Bridge = require("matrix-appservice-bridge").Bridge;
-var AppServiceRegistration = require("matrix-appservice-bridge").AppServiceRegistration;
+var ROOM_ID = '!YiuxjYhPLIZGVVkFjT:localhost'; // this room must have join_rules: public
+var Cli = require('matrix-appservice-bridge').Cli;
+var Bridge = require('matrix-appservice-bridge').Bridge;
+var AppServiceRegistration = require('matrix-appservice-bridge').AppServiceRegistration;
 
 var https = require('https');
 var roomId = process.env.ROOM_ID;
 var token = process.env.TOKEN;
-var heartbeat = " \n";
+var heartbeat = ' \n';
 var options = {
   hostname: 'stream.gitter.im',
   port: 443,
@@ -21,7 +21,7 @@ var req = https.request(options, function(res) {
     var msg = chunk.toString();
     if (msg !== heartbeat) {
       var params = JSON.parse(msg);
-      var intent = bridge.getIntent("@gitter" + params.fromUser.username + ":localhost");
+      var intent = bridge.getIntent('@gitter' + params.fromUser.username + ':localhost');
       intent.sendText(ROOM_ID, params.text);
     }
   });
@@ -32,19 +32,19 @@ req.on('error', function(e) {
 req.end();
 
 new Cli({
-  registrationPath: "gitter-registration.yaml",
+  registrationPath: 'gitter-registration.yaml',
   generateRegistration: function(reg, callback) {
     reg.setHomeserverToken(AppServiceRegistration.generateToken());
     reg.setAppServiceToken(AppServiceRegistration.generateToken());
-    reg.setSenderLocalpart("gitterbot");
-    reg.addRegexPattern("users", "@gitter_.*", true);
+    reg.setSenderLocalpart('gitterbot');
+    reg.addRegexPattern('users', '@gitter_.*', true);
     callback(reg);
   },
   run: function(port, config) {
     bridge = new Bridge({
-      homeserverUrl: "http://localhost:8008",
-      domain: "localhost",
-      registration: "gitter-registration.yaml",
+      homeserverUrl: 'http://localhost:8008',
+      domain: 'localhost',
+      registration: 'gitter-registration.yaml',
 
       controller: {
         onUserQuery: function(queriedUser) {
@@ -53,7 +53,7 @@ new Cli({
 
         onEvent: function(request, context) {
           var event = request.getData();
-          if (event.type !== "m.room.message" || !event.content || event.room_id !== ROOM_ID) {
+          if (event.type !== 'm.room.message' || !event.content || event.room_id !== ROOM_ID) {
             return;
           }
 
@@ -68,10 +68,10 @@ new Cli({
             }
           }, function(err, res) {
             if (err) {
-              console.log("HTTP Error: %s", err);
+              console.log('HTTP Error: %s', err);
             }
             else {
-              console.log("HTTP %s", res.statusCode);
+              console.log('HTTP %s', res.statusCode);
             }
           });
           postGitter.on('error', function(e) {
@@ -82,7 +82,7 @@ new Cli({
         }
       }
     });
-    console.log("Matrix-side listening on port %s", port);
+    console.log('Matrix-side listening on port %s', port);
     bridge.run(port, config);
   }
 }).run();
